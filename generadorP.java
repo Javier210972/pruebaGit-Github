df8c5bbc6322c937d85cef4fdb0e5fd58428c076
Javier <gordillosalvador72@gmail.com>
2019-10-08 23:15:55 -0600

    se creo generador de placas

---
+++ generadorP.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+
+public class generador {
+    public static void main(String[] args) {
+        String[] abc ={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        Random ran = new Random();
+        String placa = ran.nextInt(10)+""+ran.nextInt(10)+""+ran.nextInt(10)+""+abc[ran.nextInt(28)]+abc[ran.nextInt(28)]+abc[ran.nextInt(28)];
+        System.out.println(placa);
+    }
+}
